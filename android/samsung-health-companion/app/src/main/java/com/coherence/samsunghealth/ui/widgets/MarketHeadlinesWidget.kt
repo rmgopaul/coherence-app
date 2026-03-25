bcbc92c971caded79335df15eec83f4bce2ac8b8
Rhett Gopaul <rhettgopaul@192.168.1.84>
2026-03-25 08:18:47 -0500

    Remove invalid RowScope.weight import in Android market widget

--- android/samsung-health-companion/app/src/main/java/com/coherence/samsunghealth/ui/widgets/MarketHeadlinesWidget.kt
+++ android/samsung-health-companion/app/src/main/java/com/coherence/samsunghealth/ui/widgets/MarketHeadlinesWidget.kt
@@ -3,7 +3,6 @@ package com.coherence.samsunghealth.ui.widgets
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
